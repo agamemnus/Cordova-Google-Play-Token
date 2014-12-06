4de9e7df4af88f3ed3b9f9ffe153322d6f1c3020
agamemnus <agamemnus@gmail.com>
2014-12-05 19:04:49 -0500

    Update googleplaytoken.js

--- www/googleplaytoken.js
+++ www/googleplaytoken.js
@@ -1,17 +1,15 @@
 module.exports = function () {
  var exports = {}
  
- exports.initialize = function (init) {
+ exports.initialize = exports.getAccessToken = function (init) {
   var success = (typeof init.success != "undefined") ? init.success : function () {}
-  var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
-  cordova.exec (success, error, "GooglePlayToken", "tryConnect", [])
- }
- 
- exports.getAccessToken = function (init) {
-  var success = (typeof init.success != "undefined") ? init.success : function () {}
-  var error   = (typeof init.error   != "undefined") ? init.error   : function () {}
+  var error   = (typeof init.error   != "undefined") ? init.error   : function () {console.log ("error")}
   cordova.exec (success, error, "GooglePlayToken", "getAccessToken", [])
  }
  
+ exports.setScope = function (scope) {
+  if (typeof scope != "string") {console.log ("A scope parameter is required."); return}
+  cordova.exec (function () {}, function () {}, "GooglePlayToken", "setScope", [scope])
+ }
  return exports
 } ()
